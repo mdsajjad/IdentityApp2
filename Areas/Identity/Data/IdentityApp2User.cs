eca196a9c285d2e9fa5d063381eaa4a13abd28b2
mdsajjad <mdsajjadraihan@gmail.com>
2021-03-15 01:53:33 +0600

    init

---
+++ Areas/Identity/Data/IdentityApp2User.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace IdentityApp2.Areas.Identity.Data
+{
+    // Add profile data for application users by adding properties to the IdentityApp2User class
+    public class IdentityApp2User : IdentityUser
+    {
+        [PersonalData]
+        public string Name { get; set; }
+        [PersonalData]
+        public DateTime DOB { get; set; }
+    }
+}
